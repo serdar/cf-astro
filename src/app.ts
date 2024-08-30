6c926356f231cd1ba73ed48436bef0eda1209032
Serdar Kilic <serdar@kilic.net>
2024-08-30 12:41:46 +1000

    wip

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
 export function getCafe() {
   // return process.env.TESTVAR || 'Cafe1'
-  return import.meta.env.TESTVAR || 'Cafe1'
+  return import.meta.env.TESTVAR || process.env.TESTVAR || 'Cafe12'
 }
