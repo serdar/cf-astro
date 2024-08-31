175341138aad63c53e8102c847074b45e03caa6b
Serdar Kilic <serdar@kilic.net>
2024-08-31 23:30:32 +1000

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, envField } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  experimental: {
+    env: {
+      schema: {
+        TESTVAR: envField.string({ context: "server", access: "secret" }),
+      },
+    },
+  },
 });
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
-export function getCafe(): string {
-  // return process.env.TESTVAR || 'Cafe1'
-  return import.meta.env.TESTVAR || process.env.TESTVAR || "DEFAULT_CAFE";
-}
+import { TESTVAR } from "astro:env/server";
+// export function getCafe(): string {
+//   // return process.env.TESTVAR || 'Cafe1'
+//   return import.meta.env.TESTVAR || process.env.TESTVAR || "DEFAULT_CAFE";
+// }
+
+export const getCafe = () => {
+  // const client = myLib(MY_SECRET);
+  return TESTVAR;
+};
